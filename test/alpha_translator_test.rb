c156ce001bf2321a6a8e14cb216e68f19fcd795a
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-09 14:50:24 -0600

    Adds test for converting one character to alpha

--- test/alpha_translator_test.rb
+++ test/alpha_translator_test.rb
@@ -13,4 +13,13 @@ class AlphaTranslatorTest < Minitest::Test
     assert_instance_of AlphaTranslator, at
   end
 
+  def test_it_converts_one_braille_character_to_alpha
+    bt = BrailleTranslator.new
+    at = AlphaTranslator.new(bt)
+
+    actual = at.converter(".00000")
+    expected = "a"
+
+    assert_equal expected, actual
+  end
 end
