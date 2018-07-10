72f13bf3b0be6189eef8491ec45be16358087787
mmbensalah <mmbensalah@gmail.com>
2018-07-10 15:09:42 -0600

    makes 3 arrays of braille test

--- test/alpha_translator_test.rb
+++ test/alpha_translator_test.rb
@@ -7,15 +7,13 @@ require 'pry'
 class AlphaTranslatorTest < Minitest::Test
 
   def test_it_exists
-    bt = BrailleTranslator.new
-    at = AlphaTranslator.new(bt)
+    at = AlphaTranslator.new
 
     assert_instance_of AlphaTranslator, at
   end
 
   def test_it_converts_one_braille_character_to_alpha
-    bt = BrailleTranslator.new
-    at = AlphaTranslator.new(bt)
+    at = AlphaTranslator.new
 
     actual = at.converter(".00000")
     expected = "a"
@@ -24,13 +22,23 @@ class AlphaTranslatorTest < Minitest::Test
   end
 
   def test_it_converts_multiple_braille_characters_to_alpha
-    bt = BrailleTranslator.new
-    at = AlphaTranslator.new(bt)
+    at = AlphaTranslator.new
 
     actual = at.prepare_braille(".00000.00000")
     expected = "aa"
 
-    assert_equal expected, actual 
+    assert_equal expected, actual
   end
+
+  def test_makes_3_arrays_of_braille
+    at = AlphaTranslator.new
+
+    actual = at.organize_arrays([".0.0\n", "00.0\n", "0000\n"])
+    expected =[[".0.0"], ["00.0"], ["0000"]]
+
+    assert_equal expected, actual
+  end
+
+
 
 end
