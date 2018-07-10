dcf955f3879927e1911668c51b51aef66a30a235
Kathleen Yruegas <kathleenecn@gmail.com>
2018-07-10 11:16:25 -0600

    Removes BrailleTranslator object

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -3,20 +3,21 @@ require './lib/braille_translator'
 
 handle = File.open(ARGV[0], 'r')
 
-message = handle.read.chomp
+# message = handle.read.chomp
+
+lines = handle.readlines
 
 handle.close
 
-at = AlphaTranslator.new
-alpha_message = at.converter(message)
 
-writer = File.open(ARGV[1], 'w') do |f|
-  f.puts alpha_message
-end
+# at = AlphaTranslator.new
+# alpha_message = at.prepare_braille(lines)
+#
+writer = File.open(ARGV[1], 'w')
+alpha_message = "lkjsdflkjsafdlkjsdflkjdf"
 
-characters = 0
-alpha_message.each do |element|
-  characters += element.length
-end
+writer.write(alpha_message)
+
+characters = alpha_message.length
 
 puts "Created #{ARGV[1]} containing #{characters} characters."
